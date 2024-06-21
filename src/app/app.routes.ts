4b44f7833ed88fd43c32c8146d40d2a9f4c4ea4a
AndresMosquera9005 <andrubjj@gmail.com>
2024-06-21 09:14:21 -0500

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { RegistroComponent } from './components/registro/registro.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: LoginComponent
+    },
+    {
+        path: 'login', component: LoginComponent
+    },
+    {
+        path: 'registro', component: RegistroComponent
+    }
+];
