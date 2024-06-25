b6726d422c38ba59fc3c6589f3e05e962fb1fe63
AndresMosquera9005 <andrubjj@gmail.com>
2024-06-25 14:50:51 -0500

    se creo el header, falta direccionarlo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegistroComponent } from './components/registro/registro.component';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [
     {
-        path: '', component: LoginComponent
+        path: '', component: HeaderComponent
     },
     {
-        path: 'login', component: LoginComponent
+        path: 'header', component: HeaderComponent
     },
-    {
-        path: 'registro', component: RegistroComponent
-    }
+    // {
+    //     path: 'login', component: LoginComponent
+    // },
+    // {
+    //     path: 'registro', component: RegistroComponent
+    // }
 ];
